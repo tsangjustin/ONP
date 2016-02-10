31c3ec3278dde2400035a8604f24c966d810d34b
Mike Curry <mjcurry@users.noreply.github.com>
2016-02-09 23:11:15 -0500

    Create mike.c

---
+++ mike.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+  
+  char *input;
+  while (fgets(input, 100, stdin) != NULL){
+    // do something with input
+  }
+  return 0;
+
+}
