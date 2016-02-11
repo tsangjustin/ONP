4dcd57cafd7259e73ca046164a285dbf980d9b8e
Mike Curry <mjcurry@users.noreply.github.com>
2016-02-10 23:45:59 -0500

    Working code

--- mike.c
+++ mike.c
@@ -1,24 +1,80 @@
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
-#include <stdlib.h>
+
+int isOperator(char c){
+    if (c == '+' || c == '-' || c == '/' || c == '*' || c == '^'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int precedenceIsGreater(char op1, char op2){    // if op1 has greater precedence than 2
+    if (op1 == '^' && op2 != '^'){
+        return 1;
+    }
+    if (op1 == '*' || op1 == '/'){
+        if (op2 == '+' || op2 == '-') {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int main() {
-  int num;
-  int type;
-  scanf("%d", &num);  // get number of expression
-  int stackTop = -1;
-  char output[401];
-  char stack[401];
-  char input[401];
-  while (fgets(input, 401, stdin) != NULL){   // also can use "\0"
-    if (c == "("){
-      stack[stackTop++] = c;
-    }
-    else if (c == "+" || c == "-" || c == "/" || c == "*"){
-      // operator case
-    }
-  }
-  return 0;
 
+    int num;
+    scanf("%d", &num);  // get number of expression
+    getchar();  // flush newline for fgets
+    char output[401];
+    int outputTop = -1;
+
+    while (num > 0) {
+        char stack[401];
+        int stackTop = -1;
+        char input[401];
+        int curr = 0;
+        fgets(input, 401, stdin);    // take input
+        while (curr < strlen(input)-1) {   // also can use "\0"
+            if (input[curr] == '(') {
+                stack[++stackTop] = input[curr];
+            }
+            else if (isOperator(input[curr]) == 1) {
+                while (isOperator(stack[stackTop]) == 1) {
+                    if (precedenceIsGreater(stack[stackTop], input[curr]) == 1 || stack[stackTop] == input[curr]){
+                        output[++outputTop] = stack[stackTop];
+                        stackTop--;
+                    }
+                }
+                stack[++stackTop] = input[curr];
+            }
+            else if (input[curr] == ')'){
+                while(stack[stackTop] != '('){
+                    output[++outputTop] = stack[stackTop];
+                    stackTop--;
+                }
+                stackTop--;
+            }
+            else{   // operand case
+                output[++outputTop] = input[curr];
+            }
+            ++curr;
+        }
+        while(stackTop >= 0){
+            output[++outputTop] = stack[stackTop];
+            stackTop--;
+        }
+        --num;
+        if (num != 0){
+            output[++outputTop] = '\n';
+        }
+    }
+    int i = 0;
+    while(i <= outputTop){
+        printf("%c", output[i]);
+        i++;
+    }
+
+    return 0;
 }
